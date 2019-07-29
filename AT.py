817ca3284f3c32b031cd65a7ce2464ec37db9923
theomarzaki <omar.nassef@kcl.ac.uk>
2019-07-29 15:16:44 +0100

    add AT file, to send module at commands and print the result - basic

---
+++ AT.py
@@ -0,0 +1,35 @@
+import RPi.GPIO as GPIO #specific for Raspberry pi
+import serial
+import time
+
+
+class IoT():
+
+    def __init__(self,serial_dev):
+        self.device = serial.Serial(serial_dev,9600)
+        self.device.flushInput()
+
+    def Command(self,command):
+        try:
+            rec_buffer = ''
+            self.device.write((command + '\r\n').encode())
+            time.sleep(0.1)
+            if self.device.inWaiting():
+                time.sleep(0.1)
+                rec_buffer = self.device.read(self.device.inWaiting())
+            if rec_buffer != '':
+                print(rec_buffer.decode())
+                rec_buffer = ''
+        except Exception as e:
+            print(e)
+            if self.device != None:
+                self.device.close()
+            else:
+                print("no device connected")
+
+
+def main():
+    IoT("/dev/ttyAMA0").Command("AT")
+
+if __name__ == "__main__":
+    main()
