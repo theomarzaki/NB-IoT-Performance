1505ec84199a6fb80196b766b0e608d0244268be
theomarzaki <omar.nassef@kcl.ac.uk>
2019-08-06 18:49:36 +0100

    added command line arguments for specific use cases

--- main.py
+++ main.py
@@ -3,23 +3,42 @@ from GA import GA
 from utils import Evolve
 from logger import log
 from network_interface import Listener
-import configparser
+import getopt,sys,configparser
 
 
-def main():
+
+def main(argv):
     # obtaining confiurations
     config = configparser.ConfigParser()
     config.read('config.ini')
+    # obtain command line arguments. Sender --> Module sending TCP packets , Receiver --> server to recieve TCP packets sent from module
+    opts, args = getopt.getopt(argv,"sr",["sender","receiver"])
 
-    # set device based on configurations
-    # module = IoT(config.get('Module','device'),int(config.get('Module','baud_rate')))
 
-    # listener to recieve data from the module
-    client_data = Listener(config.get('Network','address'),int(config.get('Network','port')))
+    for opt, arg in opts:
+        if opt == '-s':
 
-    print("data: {}".format(client_data))
+
+            server_data = Sender(config.get('Sender','address'),int(config.get('Sender','port')))
+
+
+            # set device based on configurations
+            # module = IoT(config.get('Module','device'),int(config.get('Module','baud_rate')))
+
+        elif opt == '-r':
+
+
+
+            # listener to recieve data from the module
+            client_data = Listener(config.get('Receiver','address'),int(config.get('Receiver','port')))
+
+            print("data: {}".format(client_data))
+
+        else:
+            sys.exit()
+
 
 
 
 if __name__ == '__main__':
-    main()
+    main(sys.argv[1:])
