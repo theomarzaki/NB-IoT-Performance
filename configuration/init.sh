61729fde2a77945ee870b0d9a035286e6e31fcdc
psyy <psyy@localhost.localdomain>
2019-08-26 14:51:44 +0100

    have the container under the same bridge as host

--- configuration/init.sh
+++ configuration/init.sh
@@ -1,3 +1,3 @@
-apt-get install ppp -y
-mv configuration/ppp_options /etc/ppp/options
-mv configuration/ppp_chat_isp /etc/ppp/chat-isp
+#apt-get install ppp -y
+#mv configuration/ppp_options /etc/ppp/options
+#mv configuration/ppp_chat_isp /etc/ppp/chat-isp
--- containerise.sh
+++ containerise.sh
@@ -1,3 +1,3 @@
 docker build -t iot .
 
-docker run --privileged --device=/dev/ttyUSB0 -it iot
+docker run --privileged --net-bridge --device=/dev/ttyUSB0 -it iot
