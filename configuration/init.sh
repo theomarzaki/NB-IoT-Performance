4a99749a931a76861e5bdd7cd111f745d67d47da
psyy <psyy@localhost.localdomain>
2019-08-26 14:59:09 +0100

    add local ppp configs instead of wvdial

--- configuration/init.sh
+++ configuration/init.sh
@@ -1,3 +1,2 @@
-#apt-get install ppp -y
-#mv configuration/ppp_options /etc/ppp/options
-#mv configuration/ppp_chat_isp /etc/ppp/chat-isp
+mv configuration/ppp_options /etc/ppp/options
+mv configuration/ppp_chat_isp /etc/ppp/chat-isp
